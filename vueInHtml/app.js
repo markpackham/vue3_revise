19466360903f8481f7d8cbcd34f97d47bf399ee1
mark <markpackham1@gmail.com>
2020-12-16 16:12:22 +0000

    hide books

--- vueInHtml/app.js
+++ vueInHtml/app.js
@@ -2,15 +2,19 @@ const app = Vue.createApp({
   //   template: "<h2>I am a template</h2>",
   data() {
     return {
+      showBooks: true,
       title: "The Final Empire",
       author: "Brandon Sanderson",
       age: 30,
     };
   },
   methods: {
-    changeTitle(title) {
-      //this.title = "Title changes";
-      this.title = title;
+    // changeTitle(title) {
+    //   //this.title = "Title changes";
+    //   this.title = title;
+    // },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
   },
 });
