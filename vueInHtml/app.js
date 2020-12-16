cc70d93a360b0e8f380d0a8c6596665abc04ea9d
mark <markpackham1@gmail.com>
2020-12-16 14:53:35 +0000

    add method, https://youtu.be/F7PLPJqVotk?t=1334

--- vueInHtml/app.js
+++ vueInHtml/app.js
@@ -1,5 +1,18 @@
 const app = Vue.createApp({
   //   template: "<h2>I am a template</h2>",
+  data() {
+    return {
+      title: "The Final Empire",
+      author: "Brandon Sanderson",
+      age: 30,
+    };
+  },
+  methods: {
+    changeTitle(title) {
+      //this.title = "Title changes";
+      this.title = title;
+    },
+  },
 });
 
 app.mount("#app");
