c530278540e015e2aa5c928c6d6c7501eee2e6e2
mark <markpackham1@gmail.com>
2021-01-12 18:00:38 +0000

    add reaction timer, https://youtu.be/bc6czIBLKTg?t=780

--- modal-projet/src/App.vue
+++ modal-projet/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
-  <div v-if="showModal">
-    <Modal theme="sale" @close="toggleModal">
-      <template v-slot:links>
-        <a href="#">sign up now</a>
-        <a href="#">more info</a>
-      </template>
-      <h1>Ninja Givaway!</h1>
-      <p>Grab your ninja swag for half price!</p>
-    </Modal>
-  </div>
+  <teleport to=".modals">
+    <div v-if="showModal">
+      <Modal theme="sale" @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">sign up now</a>
+          <a href="#">more info</a>
+        </template>
+        <h1>Ninja Givaway!</h1>
+        <p>Grab your ninja swag for half price!</p>
+      </Modal>
+    </div>
+  </teleport>
   <button @click.alt="toggleModal">open modal (alt click)</button>
 </template>
 
