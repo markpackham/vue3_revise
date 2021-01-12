a28ee6dba0a83518be3b982f2aa14860cfef12f7
mark <markpackham1@gmail.com>
2021-01-12 18:23:20 +0000

    experiment with hooks, https://youtu.be/bc6czIBLKTg?t=1455

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="block">Click me</div>
+  <div class="block" v-if="showBlock">Click me</div>
 </template>
 
 <script>
 export default {
   props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  },
+  //   // updated only works if stuff in data() is changed
+  //   updated() {},
+  //   // will only show when our component is removed from Vue
+  //   unmounted() {
+  //     console.log("Unmounted");
+  //   },
+  methods: {},
 };
 </script>
 
