0a001d270b851cf040f429126782cd09a61d4721
mark <markpackham1@gmail.com>
2021-01-15 15:51:16 +0000

    setup to learn composition, https://youtu.be/V-kxBWcPJfo?t=367
    Get styling from here, https://github.com/iamshaunjp/Vue-3-Firebase/tree/lesson-100/dojo-blog

---
+++ blog-composition-api/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
