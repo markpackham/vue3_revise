dad81f8421b55decce0d1ae86d869d45ace78953
mark <markpackham1@gmail.com>
2021-01-15 12:53:50 +0000

    redirects

--- ninja-jobs/src/App.vue
+++ ninja-jobs/src/App.vue
@@ -4,8 +4,33 @@
     <router-link :to="{ name: 'About' }">About</router-link>
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </div>
+
+  <button @click="redirect">Redirect To Home</button>
+  <button @click="back">Go Back</button>
+  <button @click="forward">Go Forward</button>
+
   <router-view />
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      // send them to homepage or anypage you want to replace the route name with
+      this.$router.push({ name: "Home" });
+    },
+    back() {
+      // $router is for moving around, $route is for getting params
+      // go backwards 1 page in your browser history
+      this.$router.go(-1);
+    },
+    forward() {
+      // go forwards 1 page
+      this.$router.go(1);
+    },
+  },
+};
+</script>
 
 <style>
 #app {
