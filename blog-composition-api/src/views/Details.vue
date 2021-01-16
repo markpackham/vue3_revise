ba157fb8dfbf106acc8125bbf0dd3996ac9c2df3
mark <markpackham1@gmail.com>
2021-01-16 21:58:43 +0000

    get specific post

--- blog-composition-api/src/views/Details.vue
+++ blog-composition-api/src/views/Details.vue
@@ -1,8 +1,43 @@
-<template>details</template>
+<template>
+  <div v-if="error">{{ error }}</div>
+  <div v-if="post" class="post">
+    <h3>{{ post.title }}</h3>
+    <p class="pre">{{ post.body }}</p>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
+  </div>
+</template>
 
 <script>
-export default {};
+import getPost from "../composables/getPost";
+
+export default {
+  props: ["id"],
+  setup(props) {
+    const { post, error, load } = getPost(props.id);
+
+    load();
+
+    return { post, error };
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.tags a {
+  margin-right: 10px;
+}
+.post {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.post p {
+  color: #444;
+  line-height: 1.5em;
+  margin-top: 40px;
+}
+.pre {
+  white-space: pre-wrap;
+}
 </style>
