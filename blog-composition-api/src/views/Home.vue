0d813071baed1a45b9e2db11367ea1ec2d40b7b8
mark <markpackham1@gmail.com>
2021-01-16 13:01:32 +0000

    make refs reactive, https://youtu.be/V-kxBWcPJfo?t=1524

--- blog-composition-api/src/views/Home.vue
+++ blog-composition-api/src/views/Home.vue
@@ -1,21 +1,35 @@
 <template>
+  <!-- <p ref="paragraphTag">My name is {{ name }} my age is {{ age }}</p> -->
   <p>My name is {{ name }} my age is {{ age }}</p>
   <button @click="handleClick">Click me</button>
+  <button @click="age++">Add 1 to age</button>
+  <input type="text" v-model="name" />
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "Home",
   // setup runs before created() and mounted()
   setup() {
-    let name = "mark";
-    let age = 20;
+    const name = ref("mark");
+    const age = ref(20);
+
+    // const paragraphTag = ref(null);
+
+    // const handleClick = () => {
+    //   paragraphTag.value.classList.add("test");
+    //   paragraphTag.value.textContent = "Hello World";
+    // };
 
     const handleClick = () => {
-      console.log("You clicked me");
+      name.value = "timmy";
+      age.value = 21;
     };
 
     // you don't need to do "name: name" if key and value are the same
+    // return { name, age, handleClick, paragraphTag };
     return { name, age, handleClick };
   },
 };
